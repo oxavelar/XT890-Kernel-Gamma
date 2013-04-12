16f9e4829c8b0986f8e836b01acf690b07b5c57c
oxavelar <omar.avelar@gmail.com>
2013-04-11 22:37:55 -0500

    Going more conservative on smartassV2.

--- kernel/linux-3.0/drivers/cpufreq/cpufreq_smartass2.c
+++ kernel/linux-3.0/drivers/cpufreq/cpufreq_smartass2.c
@@ -74,13 +74,13 @@ static unsigned int ramp_down_step;
 /*
  * CPU freq will be increased if measured load > max_cpu_load;
  */
-#define DEFAULT_MAX_CPU_LOAD 80
+#define DEFAULT_MAX_CPU_LOAD 90
 static unsigned long max_cpu_load;
 
 /*
  * CPU freq will be decreased if measured load < min_cpu_load;
  */
-#define DEFAULT_MIN_CPU_LOAD 50
+#define DEFAULT_MIN_CPU_LOAD 45
 static unsigned long min_cpu_load;
 
 /*
@@ -94,7 +94,7 @@ static unsigned long up_rate_us;
  * The minimum amount of time to spend at a frequency before we can ramp down.
  * Notice we ignore this when we are above the ideal frequency.
  */
-#define DEFAULT_DOWN_RATE_US 48000;
+#define DEFAULT_DOWN_RATE_US 64000;
 static unsigned long down_rate_us;
 
 /*
