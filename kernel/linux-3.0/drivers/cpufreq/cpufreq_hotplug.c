303969a40cb9f7dbad21e7d8b2420a3b8fe18763
oxavelar <omar.avelar@gmail.com>
2013-04-28 05:19:20 -0500

    Higher sampling rates make a smoother experience.

--- kernel/linux-3.0/drivers/cpufreq/cpufreq_hotplug.c
+++ kernel/linux-3.0/drivers/cpufreq/cpufreq_hotplug.c
@@ -31,22 +31,22 @@
 #include <linux/slab.h>
 
 /* greater than 80% avg load across online CPUs increases frequency */
-#define DEFAULT_UP_FREQ_MIN_LOAD			(90)
+#define DEFAULT_UP_FREQ_MIN_LOAD			(75)
 
 /* Keep 10% of idle under the up threshold when decreasing the frequency */
 #define DEFAULT_FREQ_DOWN_DIFFERENTIAL			(10)
 
 /* less than 35% avg load across online CPUs decreases frequency */
-#define DEFAULT_DOWN_FREQ_MAX_LOAD			(35)
+#define DEFAULT_DOWN_FREQ_MAX_LOAD			(65)
 
 /* default sampling period (uSec) is bogus; 10x ondemand's default for x86 */
-#define DEFAULT_SAMPLING_PERIOD				(48000)
+#define DEFAULT_SAMPLING_PERIOD				(360000)
 
 /* default number of sampling periods to average before hotplug-in decision */
-#define DEFAULT_HOTPLUG_IN_SAMPLING_PERIODS		(7)
+#define DEFAULT_HOTPLUG_IN_SAMPLING_PERIODS		(5)
 
 /* default number of sampling periods to average before hotplug-out decision */
-#define DEFAULT_HOTPLUG_OUT_SAMPLING_PERIODS		(25)
+#define DEFAULT_HOTPLUG_OUT_SAMPLING_PERIODS		(15)
 
 static void do_dbs_timer(struct work_struct *work);
 static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
