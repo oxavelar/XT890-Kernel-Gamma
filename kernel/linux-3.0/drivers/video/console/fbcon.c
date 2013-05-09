ac719405a0172e316530be27dccabeb1d685f92e
oxavelar <omar.avelar@gmail.com>
2013-05-08 23:18:14 -0500

    Keeping 3.0.66 Kernel, and the following files intact or the camera will stop connecting.
    M	kernel/linux-3.0/include/linux/init_task.h
    M	kernel/linux-3.0/kernel/ptrace.c
    M	kernel/linux-3.0/kernel/sched.c
    M	kernel/linux-3.0/kernel/signal.c
    
    We have to keep an eye on these and eventually fix the bug. But it is no biggie for the Kernel patches.

--- kernel/linux-3.0/drivers/video/console/fbcon.c
+++ kernel/linux-3.0/drivers/video/console/fbcon.c
@@ -1229,8 +1229,6 @@ static void fbcon_deinit(struct vc_data *vc)
 finished:
 
 	fbcon_free_font(p, free_font);
-	if (free_font)
-		vc->vc_font.data = NULL;
 
 	if (!con_is_bound(&fb_con))
 		fbcon_exit();
