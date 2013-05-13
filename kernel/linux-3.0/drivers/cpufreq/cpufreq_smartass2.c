93a7192fb8a1a3f37305eb03d3cd18a1f9741da0
oxavelar <omar.avelar@gmail.com>
2013-05-12 23:36:49 -0500

    More sensitive smartassV2 and also using AtomISP2 (CSS15) instead of (CSS10).

--- kernel/linux-3.0/drivers/cpufreq/cpufreq_smartass2.c
+++ kernel/linux-3.0/drivers/cpufreq/cpufreq_smartass2.c
@@ -43,7 +43,7 @@
  * towards the ideal frequency and slower after it has passed it. Similarly,
  * lowering the frequency towards the ideal frequency is faster than below it.
  */
-#define DEFAULT_AWAKE_IDEAL_FREQ 1400000
+#define DEFAULT_AWAKE_IDEAL_FREQ 1600000
 static unsigned int awake_ideal_freq;
 
 /*
@@ -87,14 +87,14 @@ static unsigned long min_cpu_load;
  * The minimum amount of time to spend at a frequency before we can ramp up.
  * Notice we ignore this when we are below the ideal frequency.
  */
-#define DEFAULT_UP_RATE_US 140000;
+#define DEFAULT_UP_RATE_US 200000;
 static unsigned long up_rate_us;
 
 /*
  * The minimum amount of time to spend at a frequency before we can ramp down.
  * Notice we ignore this when we are above the ideal frequency.
  */
-#define DEFAULT_DOWN_RATE_US 280000;
+#define DEFAULT_DOWN_RATE_US 300000;
 static unsigned long down_rate_us;
 
 /*
