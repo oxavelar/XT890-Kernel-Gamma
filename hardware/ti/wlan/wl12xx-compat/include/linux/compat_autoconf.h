ea2ee01c1dde895063127eb004c7b131488d7dea
oxavelar <omar.avelar@gmail.com>
2013-11-29 09:55:33 -0600

    Fixing interactivex reboots.

--- hardware/ti/wlan/wl12xx-compat/include/linux/compat_autoconf.h
+++ hardware/ti/wlan/wl12xx-compat/include/linux/compat_autoconf.h
@@ -2,7 +2,7 @@
 #define COMPAT_AUTOCONF_INCLUDED
 /*
  * Automatically generated C config: don't edit
- * Sun Nov 24 15:20:04 CST 2013 
+ * Fri Nov 29 09:52:54 CST 2013 
  * compat-wireless-2.6: compat-wireless-2011-05-19-2-gfd8090a
  * linux-2.6: v2.6.39-1290-g7d5a6a7
  */
