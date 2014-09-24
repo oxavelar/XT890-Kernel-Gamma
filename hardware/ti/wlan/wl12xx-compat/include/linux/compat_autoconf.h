a508eeeae9496c56a645ab3279c71fc8545cb0eb
oxavelar <omar.avelar@gmail.com>
2014-09-23 22:57:10 -0500

    Committed the wrong file, this one is good.

--- hardware/ti/wlan/wl12xx-compat/include/linux/compat_autoconf.h
+++ hardware/ti/wlan/wl12xx-compat/include/linux/compat_autoconf.h
@@ -2,7 +2,7 @@
 #define COMPAT_AUTOCONF_INCLUDED
 /*
  * Automatically generated C config: don't edit
- * Tue Sep 23 22:38:45 CDT 2014 
+ * Tue Sep 23 22:52:02 CDT 2014 
  * compat-wireless-2.6: compat-wireless-2011-05-19-2-gfd8090a
  * linux-2.6: v2.6.39-1290-g7d5a6a7
  */
