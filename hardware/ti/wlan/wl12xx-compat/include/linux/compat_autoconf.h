1228212636d02ab75bf46fdbc65cf46ea3822f20
oxavelar <omar.avelar@gmail.com>
2014-10-05 20:07:41 -0500

    Trying max performance for a week...

--- hardware/ti/wlan/wl12xx-compat/include/linux/compat_autoconf.h
+++ hardware/ti/wlan/wl12xx-compat/include/linux/compat_autoconf.h
@@ -2,7 +2,7 @@
 #define COMPAT_AUTOCONF_INCLUDED
 /*
  * Automatically generated C config: don't edit
- * Sun Oct  5 17:07:29 CDT 2014 
+ * Sun Oct  5 20:06:14 CDT 2014 
  * compat-wireless-2.6: compat-wireless-2011-05-19-2-gfd8090a
  * linux-2.6: v2.6.39-1290-g7d5a6a7
  */
